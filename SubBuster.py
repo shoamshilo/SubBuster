2f89472510257a5c89b8c4949ceb43eda13a7ea7
shoamshilo <52922141+shoamshilo@users.noreply.github.com>
2020-12-28 17:10:42 +0200

    Update SubBuster.py

--- SubBuster.py
+++ SubBuster.py
@@ -1,25 +1,29 @@
 import sys
 import requests
-from time import sleep
+
 
 File = "" 
 domain = ""
 wordlist = ""
-
+Domains = []
 
 def StartUp():        
     global domain
     global wordlist
+    i = 0 
 
-    i=0
     for argv in sys.argv:
         if argv == "-d":
             domain = sys.argv[i + 1]
         if argv == "-w":
             wordlist = sys.argv[i + 1]
         i+=1
-    
+
+
+
+
 def BrutForce():
+    global Domains
     with open(wordlist) as File:
         line = File.readline()
         while line:
@@ -27,17 +31,18 @@ def BrutForce():
                 url = "http://" + line.strip() + "." +  domain
                 r = requests.get(url)
                 if r.status_code:
-                    print(line + " is a sub domain of " +  domain)
+                    Domains.insert(len(Domains) , url)
             except requests.exceptions.ConnectionError:   
-                print(line + " is not sub domain of " +  domain)
+                pass
             line = File.readline()
             
 
    
+if __name__ == '__main__':
+    StartUp()
+    BrutForce()
+    for url in Domains:
+        print(url)
 
-    
-    
-StartUp()
-BrutForce()
 
 
